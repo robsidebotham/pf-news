488eedf9353c06eb2eab923322acab7b553bdf20
robsidebotham <rob_sidebotham@hotmail.co.uk>
2020-05-11 09:25:00 +0100

    Setup Heroku

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,9 @@
-const knex = require("knex");
-const dbConfig = require("../knexfile");
+const ENV = process.env.NODE_ENV || 'development';
+const knex = require('knex');
+
+const dbConfig =
+  ENV === 'production'
+    ? { client: 'pg', connection: process.env.DATABASE_URL }
+    : require('../knexfile');
 
 module.exports = knex(dbConfig);
